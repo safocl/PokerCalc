f7eba59845649c272fb58430c88fc060116c9573
safocl <safocl88@gmail.com>
2019-01-28 22:28:59 +0400

    save

---
+++ git_add_all.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+git add $(find . -name '*.cpp' -or -name '*.h' -or -name '*Makefile*' -or -name 'README.md' -or -name 'LICENSE' -or -name 'PokerCalc.*' -or -name 'git_add_all.sh' -or -name '.gitignore')
