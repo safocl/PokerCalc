dd424d62cde5958771c01f99035c3e446311fe8b
safocl <safocl88@gmail.com>
2019-02-02 18:37:05 +0400

    rename count_cycles ==> cycles_count

--- include/equity.h
+++ include/equity.h
@@ -10,10 +10,10 @@
 #include "defines.h"
 
 void genBoardCards(unique_ptr<vector<Card>> && board, Deck & deck, const Hand & hero_h, const Hand & opp_h, 
-                     unique_ptr<HandStrengthList> & hsl,const int &count_cycles);
+                     unique_ptr<HandStrengthList> & hsl,const int &cycles_count);
 void parallel_genBoardCards(const Hand & hero_h, const Hand & opp_h, 
                                 const unsigned long & min_pos, const unsigned long & max_pos,
-                                unique_ptr<HandStrengthList> & hsl, const int &count_cycles);
+                                unique_ptr<HandStrengthList> & hsl, const int &cycles_count);
 void brutforcePreFlop_Flop(const Hand & hero_h, const Hand & opp_h, unique_ptr<HandStrengthList> & hsl);
 void sumHandStrength(const Hand & pl_h, const unique_ptr<vector<Card> > & board_ptr, unique_ptr<HandStrengthList> & hsl);
 
--- src/equity.cpp
+++ src/equity.cpp
@@ -13,18 +13,18 @@
 
 //---------------------------------------------------------------------------------------------------------------------------
 void genBoardCards(unique_ptr< vector<Card> > & board_ptr, unique_ptr<Deck> & deck_ptr, const Hand & hero_h, const Hand & opp_h,
-                     unique_ptr<HandStrengthList> & hsl, const int & count_cycles)
+                     unique_ptr<HandStrengthList> & hsl, const int & cycles_count)
 {
     
-    int tmp_count_cycles;
+    int tmp_cycles_count;
     deck_ptr->gen(board_ptr ,hero_h, opp_h);
     for (auto deck_el : deck_ptr->getDeckArr().operator*())
     {
         if (pushNewCardToBoard(board_ptr, hero_h, opp_h, deck_el))
         {
-            if (count_cycles > 0){
-                tmp_count_cycles = count_cycles - 1;
-                genBoardCards(board_ptr, deck_ptr, hero_h, opp_h, hsl, tmp_count_cycles);
+            if (cycles_count > 0){
+                tmp_cycles_count = cycles_count - 1;
+                genBoardCards(board_ptr, deck_ptr, hero_h, opp_h, hsl, tmp_cycles_count);
             }
             else
                 sumHandStrength(hero_h, board_ptr, hsl);
@@ -37,20 +37,20 @@ void genBoardCards(unique_ptr< vector<Card> > & board_ptr, unique_ptr<Deck> & de
 //---------------------------------------------------------------------------------------------------------------------------
 void parallel_genBoardCards(const Hand & hero_h, const Hand & opp_h, 
                               const unsigned long & min_pos, const unsigned long & max_pos,
-                              unique_ptr<HandStrengthList> & hsl, const int & count_cycles)
+                              unique_ptr<HandStrengthList> & hsl, const int & cycles_count)
 {
     unique_ptr<Deck> deck_ptr(new Deck);
     unique_ptr< vector<Card> > board_ptr(new vector<Card>);
     board_ptr->reserve(52);
-    int tmp_count_cycles;
+    int tmp_cycles_count;
     deck_ptr->gen(board_ptr ,hero_h, opp_h);
     for (unsigned long count = min_pos; count < max_pos; ++count)
     {
-        if (count_cycles > 0){
-            tmp_count_cycles = count_cycles - 2;
+        if (cycles_count > 0){
+            tmp_cycles_count = cycles_count - 2;
             if (pushNewCardToBoard(board_ptr, hero_h, opp_h, deck_ptr->getDeckArr()->at(count)))
             {
-                genBoardCards(board_ptr, deck_ptr, hero_h, opp_h, hsl, tmp_count_cycles);
+                genBoardCards(board_ptr, deck_ptr, hero_h, opp_h, hsl, tmp_cycles_count);
                 board_ptr->erase(board_ptr->end() - 1);
                 deck_ptr.get()->gen(board_ptr ,hero_h, opp_h);
             }
@@ -61,7 +61,7 @@ void parallel_genBoardCards(const Hand & hero_h, const Hand & opp_h,
 void brutforcePreFlop_Flop(const Hand &hero_h, const Hand &opp_h,
              unique_ptr<HandStrengthList> & hsl)
 {
-    int count_cycles = 3;
+    int cycles_count = 3;
 
     int max_pos;
     if (hero_h == opp_h)
@@ -71,19 +71,19 @@ void brutforcePreFlop_Flop(const Hand &hero_h, const Hand &opp_h,
     
     int min_pos1 = 0;
     int max_pos1 = max_pos / 4;
-    thread thread1(parallel_genBoardCards, hero_h, opp_h, min_pos1, max_pos1, std::ref(hsl), count_cycles);
+    thread thread1(parallel_genBoardCards, hero_h, opp_h, min_pos1, max_pos1, std::ref(hsl), cycles_count);
     
     auto min_pos2 = max_pos1;
     auto max_pos2 = max_pos1 * 2;
-    thread thread2(parallel_genBoardCards, hero_h, opp_h, min_pos2, max_pos2, std::ref(hsl), count_cycles);
+    thread thread2(parallel_genBoardCards, hero_h, opp_h, min_pos2, max_pos2, std::ref(hsl), cycles_count);
     
     auto min_pos3 = max_pos2;
     auto max_pos3 = max_pos1 * 3;
-    thread thread3(parallel_genBoardCards, hero_h, opp_h, min_pos3, max_pos3, std::ref(hsl), count_cycles);
+    thread thread3(parallel_genBoardCards, hero_h, opp_h, min_pos3, max_pos3, std::ref(hsl), cycles_count);
     
     auto min_pos4 = max_pos3;
     auto max_pos4 = max_pos;
-    thread thread4(parallel_genBoardCards, hero_h, opp_h, min_pos4, max_pos4, std::ref(hsl), count_cycles);
+    thread thread4(parallel_genBoardCards, hero_h, opp_h, min_pos4, max_pos4, std::ref(hsl), cycles_count);
 
     thread1.join();
     thread2.join();
